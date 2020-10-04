0c71fb83bcfc477bd73315aa2ea0a44d29f07d8b
Mr-Leshiy <leshiy12345678@gmail.com>
2020-10-04 17:09:20 +0300

    update

--- include/nnoops/long_arithmetic/big_decimal.hpp
+++ include/nnoops/long_arithmetic/big_decimal.hpp
@@ -11,6 +11,30 @@ namespace nnoops {
 struct BigDecimal {
   BigDecimal() = default;
 
+  BigDecimal(const BigDecimal& val)
+      : sign(val.sign),
+        integer_part(val.integer_part),
+        decimal_part(val.decimal_part) {}
+
+  BigDecimal(BigDecimal&& val)
+      : sign(std::move(val.sign)),
+        integer_part(std::move(val.integer_part)),
+        decimal_part(std::move(val.decimal_part)) {}
+
+  BigDecimal& operator=(const BigDecimal& val) {
+    this->sign = val.sign;
+    this->integer_part = val.integer_part;
+    this->decimal_part = val.decimal_part;
+    return *this;
+  }
+
+  BigDecimal& operator=(BigDecimal&& val) {
+    this->sign = std::move(val.sign);
+    this->integer_part = std::move(val.integer_part);
+    this->decimal_part = std::move(val.decimal_part);
+    return *this;
+  }
+
   BigDecimal(const std::string& val);
 
   BigDecimal(const double& val);
@@ -18,6 +42,8 @@ struct BigDecimal {
   friend std::string toPrettyString(const BigDecimal& val);
 
  private:
+  void optimizeDecimalPart();
+
   bool sign{true};
   uint64_t integer_part{0};
   uint64_t decimal_part{0};
