b0593c171bc25592136540d9e050ff232642727a
Mr-Leshiy <leshiy12345678@gmail.com>
2020-10-16 23:11:14 +0300

    update

---
+++ include/nnoops/long_arithmetic/unsigned_big_integer.hpp
@@ -0,0 +1,178 @@
+#ifndef NNOOPS_LIB_CPP_LONG_ARITHMETIC_BIG_INTEGER_HPP_
+#define NNOOPS_LIB_CPP_LONG_ARITHMETIC_BIG_INTEGER_HPP_
+
+#include <stdint.h>
+
+#include <array>
+#include <string>
+#include <type_traits>
+
+#include "nnoops/strutil.hpp"
+
+namespace nnoops {
+
+// Representation on the unsigned integer with the arbitrary size
+// SIZE should be multiple of 8 (1 byte)
+template <uint64_t SIZE = 64,
+          typename = typename std::enable_if<SIZE % 8 == 0 && SIZE != 0>::type>
+struct UBigInteger {
+  UBigInteger() = default;
+
+  UBigInteger(const UBigInteger<SIZE>& val) : data(val.data) {}
+
+  UBigInteger(UBigInteger<SIZE>&& val) : data(std::move(val.data)) {}
+
+  UBigInteger<SIZE>& operator=(const UBigInteger<SIZE>& val) {
+    this->data = val.data;
+    return *this;
+  }
+
+  UBigInteger<SIZE>& operator=(UBigInteger<SIZE>&& val) {
+    this->data = std::move(val.data);
+    return *this;
+  }
+
+  UBigInteger(uint8_t val) { init(val); }
+
+  UBigInteger(uint16_t val) { init(val); }
+
+  UBigInteger(uint32_t val) { init(val); }
+
+  UBigInteger(uint64_t val) { init(val); }
+
+  UBigInteger(int8_t val) {
+    assert(val >= 0 && "value should be positive");
+    init((uint8_t)val);
+  }
+
+  UBigInteger(int16_t val) {
+    assert(val >= 0 && "value should be positive");
+    init((uint16_t)val);
+  }
+
+  UBigInteger(int32_t val) {
+    assert(val >= 0 && "value should be positive");
+    init((uint32_t)val);
+  }
+
+  UBigInteger(int64_t val) {
+    assert(val >= 0 && "value should be positive");
+    init((uint64_t)val);
+  }
+
+  UBigInteger<SIZE> operator-() const {
+    UBigInteger<SIZE> ret = *this;
+    for (size_t i = 0; i < data.size(); ++i) {
+      ret.data[i] = ~this->data[i];
+    }
+    return ++ret;
+  }
+
+  UBigInteger<SIZE> operator++() {
+    // prefix operator
+    for (size_t i = 0; i < data.size() && ++data[i] == 0; ++i) {
+    }
+  }
+
+  UBigInteger<SIZE> operator++(int) {
+    // postfix operator
+    UBigInteger<SIZE> ret = *this;
+    ++(*this);
+    return ret;
+  }
+
+  UBigInteger<SIZE>& operator+=(const UBigInteger<SIZE>& b) {
+    uint64_t carry = 0;
+    for (size_t i = 0; i < data.size(); ++i) {
+      uint64_t n = carry + this->data[i] + b.data[i];
+      this->data[i] = n & 0xff;
+      carry = n >> 8;
+    }
+    return *this;
+  }
+
+  UBigInteger<SIZE> operator-=(const UBigInteger<SIZE>& b) {
+    return *this += -b;
+  }
+
+  friend inline UBigInteger<SIZE> operator+(const UBigInteger<SIZE>& a,
+                                            const UBigInteger<SIZE>& b) {
+    return UBigInteger<SIZE>(a) += b;
+  }
+
+  friend inline UBigInteger<SIZE> operator-(const UBigInteger<SIZE>& a,
+                                            const UBigInteger<SIZE>& b) {
+    return UBigInteger<SIZE>(a) -= b;
+  }
+
+  bool operator==(const UBigInteger<SIZE>& val) const {
+    return this->data == val.data;
+  }
+
+  bool operator!=(const UBigInteger<SIZE>& val) const {
+    return !(*this == val);
+  }
+
+  // return -1 if this less than b,
+  // return 1 if this bigger than b
+  // return 0 if this equal to b
+  int compareTo(const UBigInteger<SIZE>& b) const {
+    for (int64_t i = (int64_t)(data.size() - 1); i != 0; --i) {
+      if (this->data[i] < b.data[i]) {
+        return -1;
+      }
+      if (this->data[i] > b.data[i]) {
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+
+  friend bool operator>(const UBigInteger<SIZE>& a,
+                        const UBigInteger<SIZE>& b) {
+    return a.compareTo(b) > 0;
+  }
+
+  friend bool operator<(const UBigInteger<SIZE>& a,
+                        const UBigInteger<SIZE>& b) {
+    return a.compareTo(b) < 0;
+  }
+
+  friend bool operator>=(const UBigInteger<SIZE>& a,
+                         const UBigInteger<SIZE>& b) {
+    return a.compareTo(b) >= 0;
+  }
+
+  friend bool operator<=(const UBigInteger<SIZE>& a,
+                         const UBigInteger<SIZE>& b) {
+    return a.compareTo(b) <= 0;
+  }
+
+  friend std::string toPrettyString(const UBigInteger<SIZE>& val) {
+    return HexStr(val.data.rbegin(), val.data.rend());
+  }
+
+ protected:
+  template <typename T,
+            typename = typename std::enable_if<
+                std::is_integral<T>::value && std::is_unsigned<T>::value>::type>
+  void init(T value) {
+    size_t val_size = sizeof(value);
+    assert(data.size() >= val_size && "data has a small size");
+    for (size_t i = 0; i < data.size(); ++i) {
+      if (i < val_size) {
+        data[i] = (uint8_t)(value >> 8 * i);
+      } else {
+        data[i] = 0;
+      }
+    }
+  }
+
+ private:
+  std::array<uint8_t, SIZE / 8> data{};
+};
+
+}  // namespace nnoops
+
+#endif
--- test/long_arithmetic/unsigned_big_integer_test.cpp
+++ test/long_arithmetic/unsigned_big_integer_test.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 
-#include "nnoops/long_arithmetic/big_integer.hpp"
+#include "nnoops/long_arithmetic/unsigned_big_integer.hpp"
 
 using namespace nnoops;
 
-TEST(BigInteger, comparison_test) {
-  BigInteger val1(-1234);
-  BigInteger val2(1234);
+TEST(UBigInteger, comparison_test) {
+  UBigInteger val1(431);
+  UBigInteger val2(25423);
 
   EXPECT_TRUE(val1 == val1);
   EXPECT_FALSE(val1 == val2);
@@ -34,28 +34,14 @@ TEST(BigInteger, comparison_test) {
   EXPECT_TRUE(val2 >= val2);
 }
 
-TEST(BigInteger, addition_test) {
-  BigInteger val1(-1234);
-  BigInteger val2(1234);
+TEST(UBigInteger, addition_test) {}
 
-  BigInteger res;
+TEST(UBigInteger, toPrettyString_test) {
+  UBigInteger<32> val(1234);
 
-  res = val1 + val1;
+  EXPECT_EQ(toPrettyString(val), "000004d2");
 
-  EXPECT_EQ(toPrettyString(res), toPrettyString(BigInteger(-2468)));
-
-  res += 1234;
-
-  EXPECT_EQ(toPrettyString(res), toPrettyString(BigInteger(-1234)));
-
-}
-
-TEST(BigInteger, toPrettyString_test) {
-  BigInteger<32> val(-1234);
-
-  EXPECT_EQ(toPrettyString(val), "-000004d2");
-
-  val = BigInteger<32>(1235);
+  val = UBigInteger<32>(1235);
 
   EXPECT_EQ(toPrettyString(val), "000004d3");
 }
