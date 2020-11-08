7c89adcbc1ac6103245c24eca2c1ec27be6b2259
Mr-Leshiy <leshiy12345678@gmail.com>
2020-11-08 15:59:42 +0200

    update

--- test/long_arithmetic/unsigned_big_integer_heavy_test.cpp
+++ test/long_arithmetic/unsigned_big_integer_heavy_test.cpp
@@ -5,6 +5,41 @@
 
 using namespace nnoops;
 
+void addition_test(uint64_t a, uint64_t b) {
+  a = (uint32_t)a;
+  b = (uint32_t)b;
+  UBigInteger<64> val1 = a;
+  UBigInteger<64> val2 = b;
+  uint64_t c = a + b;
+  EXPECT_EQ(val1 + val2, UBigInteger<64>(c));
+  EXPECT_EQ(val2 + val1, UBigInteger<64>(c));
+}
+
+void substraction_test(uint64_t a, uint64_t b) {
+  UBigInteger<64> val1 = a;
+  UBigInteger<64> val2 = b;
+  uint64_t c = a - b;
+  EXPECT_EQ(val1 - val2, UBigInteger<64>(c));
+  EXPECT_EQ(-(val2 - val1), UBigInteger<64>(c));
+  c = b - a;
+  EXPECT_EQ(val2 - val1, UBigInteger<64>(c));
+  EXPECT_EQ(-(val1 - val2), UBigInteger<64>(c));
+  EXPECT_EQ(val2 -= val1, UBigInteger<64>(c));
+
+  EXPECT_EQ(val1 - val1, UBigInteger<64>(0));
+  EXPECT_EQ(val2 - val2, UBigInteger<64>(0));
+}
+
+void multiplication_test(uint64_t a, uint64_t b) {
+  a = (uint32_t)a;
+  b = (uint32_t)b;
+  UBigInteger<64> val1 = a;
+  UBigInteger<64> val2 = b;
+  uint64_t c = a * b;
+  EXPECT_EQ(val1 * val2, UBigInteger<64>(c));
+  EXPECT_EQ(val2 * val1, UBigInteger<64>(c));
+}
+
 void division_check1(uint64_t a, uint64_t b) {
   UBigInteger<64> val1 = a;
   UBigInteger<64> val2 = b;
@@ -27,54 +62,73 @@ void division_check2(uint64_t a, uint64_t b) {
   EXPECT_EQ(val2 % val2, UBigInteger<64>(0));
 }
 
-TEST(UBigInteger, heavy_division_test) {
-  for (uint64_t a = 1; a < 1000; ++a) {
-    for (uint64_t b = 1; b < 1000; ++b) {
-      division_check1(a, b);
+struct UBigIntegerHeavyTestCase {
+  uint64_t a_min;
+  uint64_t a_max;
+  uint64_t b_min;
+  uint64_t b_max;
+};
+
+struct UBigIntegerHeavyTest
+    : public testing::TestWithParam<UBigIntegerHeavyTestCase> {};
+
+TEST_P(UBigIntegerHeavyTest, heavy_addition_test) {
+  auto value = GetParam();
+  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
+    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
+      addition_test(a, b);
     }
   }
+}
 
-  for (uint64_t a = (std::numeric_limits<uint64_t>::max() - 1000);
-       a < std::numeric_limits<uint64_t>::max();
-       ++a) {
-    for (uint64_t b = (std::numeric_limits<uint64_t>::max() - 1000);
-         b < std::numeric_limits<uint64_t>::max();
-         ++b) {
-      division_check1(a, b);
+TEST_P(UBigIntegerHeavyTest, heavy_substraction_test) {
+  auto value = GetParam();
+  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
+    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
+      substraction_test(a, b);
     }
   }
+}
 
-  for (uint64_t a = (std::numeric_limits<uint64_t>::max() - 1000);
-       a < std::numeric_limits<uint64_t>::max();
-       ++a) {
-    for (uint64_t b = 1; b < 1000; ++b) {
+TEST_P(UBigIntegerHeavyTest, heavy_multiplication_test) {
+  auto value = GetParam();
+  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
+    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
+      multiplication_test(a, b);
+    }
+  }
+}
+
+TEST_P(UBigIntegerHeavyTest, heavy_division_test) {
+  auto value = GetParam();
+  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
+    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       division_check1(a, b);
     }
   }
 }
 
-TEST(UBigInteger, heavy_division_test2) {
-  for (uint64_t a = 1; a < 1000; ++a) {
-    for (uint64_t b = 1; b < 1000; ++b) {
-      division_check2(a, b);
-    }
-  }
-
-  for (uint64_t a = (std::numeric_limits<uint64_t>::max() - 1000);
-       a < std::numeric_limits<uint64_t>::max();
-       ++a) {
-    for (uint64_t b = (std::numeric_limits<uint64_t>::max() - 1000);
-         b < std::numeric_limits<uint64_t>::max();
-         ++b) {
-      division_check2(a, b);
-    }
-  }
-
-  for (uint64_t a = (std::numeric_limits<uint64_t>::max() - 1000);
-       a < std::numeric_limits<uint64_t>::max();
-       ++a) {
-    for (uint64_t b = 1; b < 1000; ++b) {
+TEST_P(UBigIntegerHeavyTest, heavy_division_test2) {
+  auto value = GetParam();
+  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
+    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       division_check2(a, b);
     }
   }
 }
+
+static std::vector<UBigIntegerHeavyTestCase> test_cases = {
+    {1, 1000, 1, 1000},
+    {(std::numeric_limits<uint64_t>::max() - 1000),
+     std::numeric_limits<uint64_t>::max(),
+     (std::numeric_limits<uint64_t>::max() - 1000),
+     std::numeric_limits<uint64_t>::max()},
+    {(std::numeric_limits<uint64_t>::max() - 1000),
+     std::numeric_limits<uint64_t>::max(),
+     1,
+     1000},
+};
+
+INSTANTIATE_TEST_SUITE_P(UBigIntegerHeavyTestSuite,
+                         UBigIntegerHeavyTest,
+                         testing::ValuesIn(test_cases));
