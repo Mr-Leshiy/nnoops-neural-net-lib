42c69bb6a63c59da19bc59c0c0608ef682b7fc5e
Alexey Poghilenkov <alex_pozhilenkov@adoriasoft.com>
2020-11-10 22:30:40 +0200

    update

--- test/long_arithmetic/signed_big_integer_test.cpp
+++ test/long_arithmetic/signed_big_integer_test.cpp
@@ -4,9 +4,19 @@
 
 using namespace nnoops;
 
-TEST(BigInteger, comparison_test) {
-  BigInteger val1(431);
-  BigInteger val2(25423);
+template <uint64_t SIZE>
+struct BigIntegerTestCase {
+  const static uint64_t size = SIZE;
+};
+
+template <typename T>
+struct BigIntegerTest : public ::testing::Test {};
+
+TYPED_TEST_SUITE_P(BigIntegerTest);
+TYPED_TEST_P(BigIntegerTest, comparison_test) {
+  const static uint64_t size = TypeParam::size;
+  BigInteger<size> val1(431);
+  BigInteger<size> val2(25423);
 
   EXPECT_TRUE(val1 == val1);
   EXPECT_FALSE(val1 == val2);
@@ -90,72 +100,96 @@ TEST(BigInteger, comparison_test) {
   EXPECT_TRUE(val2 >= val2);
 }
 
-TEST(BigInteger, addition_substraction_test) {
-  BigInteger val1(66576);
-  BigInteger val2(53648);
+TYPED_TEST_P(BigIntegerTest, addition_substraction_test) {
+  const static uint64_t size = TypeParam::size;
+  BigInteger<size> val1(66576);
+  BigInteger<size> val2(53648);
 
   val1 += val2;
 
-  EXPECT_EQ(val1, BigInteger(120224));
+  EXPECT_EQ(val1, BigInteger<size>(120224));
 
   val1 -= val2;
 
-  EXPECT_EQ(val1, BigInteger(66576));
+  EXPECT_EQ(val1, BigInteger<size>(66576));
 
-  EXPECT_EQ(val1 + val2, BigInteger(120224));
-  EXPECT_EQ(val1 + val2, BigInteger(120224));
+  EXPECT_EQ(val1 + val2, BigInteger<size>(120224));
+  EXPECT_EQ(val1 + val2, BigInteger<size>(120224));
 
-  EXPECT_EQ(val1 - val2, BigInteger(12928));
-  EXPECT_EQ(val2 - val1, BigInteger(-12928));
+  EXPECT_EQ(val1 - val2, BigInteger<size>(12928));
+  EXPECT_EQ(val2 - val1, BigInteger<size>(-12928));
 
-  EXPECT_EQ(val1++, BigInteger(66576));
-  EXPECT_EQ(val1, BigInteger(66577));
+  EXPECT_EQ(val1++, BigInteger<size>(66576));
+  EXPECT_EQ(val1, BigInteger<size>(66577));
 
-  EXPECT_EQ(++val1, BigInteger(66578));
-  EXPECT_EQ(val1, BigInteger(66578));
+  EXPECT_EQ(++val1, BigInteger<size>(66578));
+  EXPECT_EQ(val1, BigInteger<size>(66578));
 
-  EXPECT_EQ(val1--, BigInteger(66578));
-  EXPECT_EQ(val1, BigInteger(66577));
+  EXPECT_EQ(val1--, BigInteger<size>(66578));
+  EXPECT_EQ(val1, BigInteger<size>(66577));
 
-  EXPECT_EQ(--val1, BigInteger(66576));
-  EXPECT_EQ(val1, BigInteger(66576));
+  EXPECT_EQ(--val1, BigInteger<size>(66576));
+  EXPECT_EQ(val1, BigInteger<size>(66576));
 
   val1 = -12928;
 
-  EXPECT_EQ(val1--, BigInteger(-12928));
-  EXPECT_EQ(val1, BigInteger(-12929));
+  EXPECT_EQ(val1--, BigInteger<size>(-12928));
+  EXPECT_EQ(val1, BigInteger<size>(-12929));
 
-  EXPECT_EQ(--val1, BigInteger(-12930));
-  EXPECT_EQ(val1, BigInteger(-12930));
+  EXPECT_EQ(--val1, BigInteger<size>(-12930));
+  EXPECT_EQ(val1, BigInteger<size>(-12930));
 
-  EXPECT_EQ(val1++, BigInteger(-12930));
-  EXPECT_EQ(val1, BigInteger(-12929));
+  EXPECT_EQ(val1++, BigInteger<size>(-12930));
+  EXPECT_EQ(val1, BigInteger<size>(-12929));
 
-  EXPECT_EQ(++val1, BigInteger(-12928));
-  EXPECT_EQ(val1, BigInteger(-12928));
+  EXPECT_EQ(++val1, BigInteger<size>(-12928));
+  EXPECT_EQ(val1, BigInteger<size>(-12928));
 
   val1 += val1;
 
-  EXPECT_EQ(val1, BigInteger(-25856));
+  EXPECT_EQ(val1, BigInteger<size>(-25856));
 
-  val1 -= BigInteger(-12928);
+  val1 -= BigInteger<size>(-12928);
 
-  EXPECT_EQ(val1, BigInteger(-12928));
+  EXPECT_EQ(val1, BigInteger<size>(-12928));
 
-  EXPECT_EQ(val1 + val1, BigInteger(-25856));
-  EXPECT_EQ(val1 - val1, BigInteger(0));
+  EXPECT_EQ(val1 + val1, BigInteger<size>(-25856));
+  EXPECT_EQ(val1 - val1, BigInteger<size>(0));
 
   val2 = 12928 * 2;
 
-  EXPECT_EQ(val1 - val2, BigInteger(-38784));
-  EXPECT_EQ(val1 + val2, BigInteger(12928));
+  EXPECT_EQ(val1 - val2, BigInteger<size>(-38784));
+  EXPECT_EQ(val1 + val2, BigInteger<size>(12928));
 }
 
-TEST(BigInteger, multiplication_test) {}
+TYPED_TEST_P(BigIntegerTest, multiplication_test) {}
 
-TEST(BigInteger, division_test) {}
+TYPED_TEST_P(BigIntegerTest, division_test) {}
 
-TEST(BigInteger, division_test2) {}
+TYPED_TEST_P(BigIntegerTest, division_test2) {}
+
+TYPED_TEST_P(BigIntegerTest, exception_handling_test) {}
+
+REGISTER_TYPED_TEST_SUITE_P(BigIntegerTest,
+                            comparison_test,
+                            addition_substraction_test,
+                            multiplication_test,
+                            division_test,
+                            division_test2,
+                            exception_handling_test);
+
+typedef ::testing::Types<BigIntegerTestCase<64>,
+                         BigIntegerTestCase<128>,
+                         BigIntegerTestCase<256>,
+                         BigIntegerTestCase<512>,
+                         BigIntegerTestCase<1024>,
+                         BigIntegerTestCase<2048>,
+                         BigIntegerTestCase<4096>>
+    BigIntegerTestCases;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(BigIntegerTestSuite,
+                               BigIntegerTest,
+                               BigIntegerTestCases);
 
 TEST(BigInteger, toPrettyString_test) {
   BigInteger<32> val(1234);
