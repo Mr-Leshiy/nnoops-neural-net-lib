07bf4a00ec0e732dbc703dc5f3a068da85215386
Mr-Leshiy <leshiy12345678@gmail.com>
2020-11-12 00:21:08 +0200

    update tests

--- test/long_arithmetic/unsigned_big_integer_heavy_test.cpp
+++ test/long_arithmetic/unsigned_big_integer_heavy_test.cpp
@@ -33,9 +33,8 @@ void addition_check(uint64_t a, uint64_t b) {
   b = (uint32_t)b;
   UBigInteger<64> val1 = a;
   UBigInteger<64> val2 = b;
-  uint64_t c = a + b;
-  EXPECT_EQ(val1 + val2, UBigInteger<64>(c));
-  EXPECT_EQ(val2 + val1, UBigInteger<64>(c));
+  EXPECT_EQ(val1 + val2, UBigInteger<64>(a + b));
+  EXPECT_EQ(val2 + val1, UBigInteger<64>(a + b));
 }
 
 void substraction_check(uint64_t a, uint64_t b) {
@@ -56,29 +55,30 @@ void multiplication_check(uint64_t a, uint64_t b) {
   b = (uint32_t)b;
   UBigInteger<64> val1 = a;
   UBigInteger<64> val2 = b;
-  uint64_t c = a * b;
-  EXPECT_EQ(val1 * val2, UBigInteger<64>(c));
-  EXPECT_EQ(val2 * val1, UBigInteger<64>(c));
+  EXPECT_EQ(val1 * val2, UBigInteger<64>(a * b));
+  EXPECT_EQ(val2 * val1, UBigInteger<64>(a * b));
 }
 
 void division_check1(uint64_t a, uint64_t b) {
+  if (a == 0 || b == 0) {
+    return;
+  }
   UBigInteger<64> val1 = a;
   UBigInteger<64> val2 = b;
-  uint64_t c = a / b;
-  EXPECT_EQ(val1 / val2, UBigInteger<64>(c));
-  c = b / a;
-  EXPECT_EQ(val2 / val1, UBigInteger<64>(c));
+  EXPECT_EQ(val1 / val2, UBigInteger<64>(a / b));
+  EXPECT_EQ(val2 / val1, UBigInteger<64>(b / a));
   EXPECT_EQ(val1 / val1, UBigInteger<64>(1));
   EXPECT_EQ(val2 / val2, UBigInteger<64>(1));
 }
 
 void division_check2(uint64_t a, uint64_t b) {
+  if (a == 0 || b == 0) {
+    return;
+  }
   UBigInteger<64> val1 = a;
   UBigInteger<64> val2 = b;
-  uint64_t c = a % b;
-  EXPECT_EQ(val1 % val2, UBigInteger<64>(c));
-  c = b % a;
-  EXPECT_EQ(val2 % val1, UBigInteger<64>(c));
+  EXPECT_EQ(val1 % val2, UBigInteger<64>(a % b));
+  EXPECT_EQ(val2 % val1, UBigInteger<64>(b % a));
   EXPECT_EQ(val1 % val1, UBigInteger<64>(0));
   EXPECT_EQ(val2 % val2, UBigInteger<64>(0));
 }
@@ -93,69 +93,29 @@ struct UBigIntegerHeavyTestCase {
 struct UBigIntegerHeavyTest
     : public testing::TestWithParam<UBigIntegerHeavyTestCase> {};
 
-TEST_P(UBigIntegerHeavyTest, heavy_comparison_test) {
+TEST_P(UBigIntegerHeavyTest, heavy_complex_test) {
   auto value = GetParam();
   for (uint64_t a = value.a_min; a < value.a_max; ++a) {
     for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       comparison_check(a, b);
-    }
-  }
-}
-
-TEST_P(UBigIntegerHeavyTest, heavy_addition_test) {
-  auto value = GetParam();
-  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
-    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       addition_check(a, b);
-    }
-  }
-}
-
-TEST_P(UBigIntegerHeavyTest, heavy_substraction_test) {
-  auto value = GetParam();
-  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
-    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       substraction_check(a, b);
-    }
-  }
-}
-
-TEST_P(UBigIntegerHeavyTest, heavy_multiplication_test) {
-  auto value = GetParam();
-  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
-    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       multiplication_check(a, b);
-    }
-  }
-}
-
-TEST_P(UBigIntegerHeavyTest, heavy_division_test) {
-  auto value = GetParam();
-  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
-    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       division_check1(a, b);
-    }
-  }
-}
-
-TEST_P(UBigIntegerHeavyTest, heavy_division_test2) {
-  auto value = GetParam();
-  for (uint64_t a = value.a_min; a < value.a_max; ++a) {
-    for (uint64_t b = value.b_min; b < value.b_max; ++b) {
       division_check2(a, b);
     }
   }
 }
 
 static std::vector<UBigIntegerHeavyTestCase> test_cases = {
-    {1, 1000, 1, 1000},
+    {0, 1000, 0, 1000},
     {(std::numeric_limits<uint64_t>::max() - 1000),
      std::numeric_limits<uint64_t>::max(),
      (std::numeric_limits<uint64_t>::max() - 1000),
      std::numeric_limits<uint64_t>::max()},
     {(std::numeric_limits<uint64_t>::max() - 1000),
      std::numeric_limits<uint64_t>::max(),
-     1,
+     0,
      1000},
 };
 
