9c158a7bf02a3699ce74634c5d21e54ba16d55ff
Mr-Leshiy <leshiy12345678@gmail.com>
2020-11-27 01:55:45 +0200

    update

--- include/nnoops/long_arithmetic/unsigned_big_integer.hpp
+++ include/nnoops/long_arithmetic/unsigned_big_integer.hpp
@@ -415,14 +415,25 @@ struct UBigInteger {
 
 extern template struct UBigInteger<8, uint8_t>;
 extern template struct UBigInteger<16, uint8_t>;
-extern template struct UBigInteger<32>;
-extern template struct UBigInteger<64>;
-extern template struct UBigInteger<128>;
-extern template struct UBigInteger<256>;
-extern template struct UBigInteger<512>;
-extern template struct UBigInteger<1024>;
-extern template struct UBigInteger<2048>;
-extern template struct UBigInteger<4096>;
+extern template struct UBigInteger<32, uint8_t>;
+extern template struct UBigInteger<64, uint8_t>;
+extern template struct UBigInteger<128, uint8_t>;
+extern template struct UBigInteger<256, uint8_t>;
+extern template struct UBigInteger<512, uint8_t>;
+extern template struct UBigInteger<1024, uint8_t>;
+extern template struct UBigInteger<2048, uint8_t>;
+extern template struct UBigInteger<4096, uint8_t>;
+
+extern template struct UBigInteger<8, uint32_t>;
+extern template struct UBigInteger<16, uint32_t>;
+extern template struct UBigInteger<32, uint32_t>;
+extern template struct UBigInteger<64, uint32_t>;
+extern template struct UBigInteger<128, uint32_t>;
+extern template struct UBigInteger<256, uint32_t>;
+extern template struct UBigInteger<512, uint32_t>;
+extern template struct UBigInteger<1024, uint32_t>;
+extern template struct UBigInteger<2048, uint32_t>;
+extern template struct UBigInteger<4096, uint32_t>;
 
 }  // namespace nnoops
 
